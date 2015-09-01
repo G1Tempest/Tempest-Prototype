6f3743d37fe6798a13d2c53414b9352f32f3ad25
yashasg <yashas.gujjar@gmail.com>
2015-08-31 20:51:32 -0600

    Testing

--- Idk/Idk/Game1.cs
+++ Idk/Idk/Game1.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
+using System.Text;
+using FarseerPhysics.Common;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Joints;
+
 
 namespace Idk
 {
@@ -11,11 +18,23 @@ namespace Idk
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        AnimatedSprite car1,car2;
+        float timer;
+        string direction = "down";
+        World world;
+
+        //Physics parameters
+
+        const float MaxSteerAngle = (float)Math.PI / 3;
+        const float SteerSpeed = 1.5f;
+        const float Horsepower = 40f;
+        Vector2 carStartingPos = new Vector2(, 10);
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            world = new World(new Vector2(0,0));
         }
 
         /// <summary>
@@ -39,6 +58,14 @@ namespace Idk
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+           
+           Texture2D cartex = Content.Load<Texture2D>("car");
+            car1 = new AnimatedSprite(cartex, 4, 3,new Vector2(400,200));
+            car2 = new AnimatedSprite(cartex, 4, 3, new Vector2(200, 300));
+
+            //background
+            //const string bgImagePath = "";
+            //backgroundTexture = Content.Load<Texture2D>(bgImagePath);
 
             // TODO: use this.Content to load your game content here
         }
@@ -61,6 +88,96 @@ namespace Idk
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if(timer>=1.0f)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                {
+                    if (!direction.Equals("down"))
+                    {
+                        direction = "down";
+                        
+                    }
+                    car1.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                {
+                    if (!direction.Equals("up"))
+                    {
+                        direction = "up";
+
+                    }
+                    car1.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                {
+                    if (!direction.Equals("left"))
+                    {
+                        direction = "left";
+
+                    }
+                    car1.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                {
+                    if (!direction.Equals("right"))
+                    {
+                        direction = "right";
+
+                    }
+                    car1.Update(direction);
+
+                }
+
+
+                //player 2
+                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                {
+                    if (!direction.Equals("down"))
+                    {
+                        direction = "down";
+
+                    }
+                    car2.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.W))
+                {
+                    if (!direction.Equals("up"))
+                    {
+                        direction = "up";
+
+                    }
+                    car2.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.A))
+                {
+                    if (!direction.Equals("left"))
+                    {
+                        direction = "left";
+
+                    }
+                    car2.Update(direction);
+
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.D))
+                {
+                    if (!direction.Equals("right"))
+                    {
+                        direction = "right";
+
+                    }
+                    car2.Update(direction);
+
+                }
+
+
+
+            }
 
             // TODO: Add your update logic here
 
@@ -74,6 +191,10 @@ namespace Idk
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+                       car1.Draw(spriteBatch);
+            car2.Draw(spriteBatch);
+            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
